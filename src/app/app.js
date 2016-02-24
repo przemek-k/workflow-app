17f5aad590fda3cd57ef25cc842de0abea98ca7f
Przemek <przemyslaw.kalka@gmail.com>
2016-02-24 07:06:56 +0000

    Removing unused icons

--- src/app/app.js
+++ src/app/app.js
@@ -100,19 +100,14 @@ app.config(function($mdThemingProvider, $mdIconProvider){
 
   $mdIconProvider
     .icon("account"     , "dist/assets/svg/account_circle.svg"       , 36)
-    .icon("cached"      , "dist/src/assets/svg/cached.svg"           , 36)
     .icon("dashboard"   , "dist/assets/svg/dashboard.svg"            , 36)
     .icon("hub"         , "dist/assets/svg/device_hub.svg"           , 36)
     .icon("logout"      , "dist/assets/svg/exit_to_app.svg"          , 36)
-    .icon("face"        , "dist/assets/svg/face_black.svg"           , 36)
     .icon("notification", "dist/assets/svg/notifications.svg"        , 36)
-    .icon("app-settings", "dist/assets/svg/settings_applications.svg", 36)
     .icon("settings"    , "dist/assets/svg/settings.svg"             , 36)
     .icon("menu"        , "dist/assets/svg/menu.svg"                 , 36)
-    .icon("super-user"  , "dist/assets/svg/supervisor_account.svg"   , 36)
     .icon("reports"     , "dist/assets/svg/trending_up.svg"          , 36)
-    .icon("widgets"     , "dist/assets/svg/widgets.svg"          , 36)
-    .icon("timeline"    , "dist/assets/svg/timeline.svg"             , 36);
+    .icon("widgets"     , "dist/assets/svg/widgets.svg"              , 36);
 
   $mdThemingProvider.theme('default')
     .primaryPalette('blue')
